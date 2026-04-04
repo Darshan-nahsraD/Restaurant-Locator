10f3445a65a763dac2e1a4329bbf370406e8376f
Darshan Mujalde <darshanmujaldepc@gmail.com>
2026-04-04 23:05:18 +0530

    main file

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "range_tree.h"
+
+using namespace std;
+
+int main() {
+
+    vector<Point> restaurants = {
+        {1,2}, {3,4}, {5,6}, {7,8}, {2,5}, {6,3}
+    };
+
+    RangeTree tree;
+    tree.build(restaurants);
+
+    int x1 = 2, x2 = 6;
+    int y1 = 3, y2 = 6;
+
+    auto res = tree.query(x1, x2, y1, y2);
+
+    cout << "Restaurants in range:\n";
+
+    for (auto p : res) {
+        cout << "(" << p.x << ", " << p.y << ")\n";
+    }
+}
